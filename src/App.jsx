39c311cbf1f359fa6f2c0279398c5b5386203383
samar3366 <sumanthreddypathi3366@gmail.com>
2022-11-10 00:08:39 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,70 @@
 import React from "react";
 
 import Layout from "./layout";
-import {Home} from './pages';
+import { Home } from "./pages";
 
 const App = (props) => {
-  return <Layout>
-    <Home />
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-    <h1>Sanskar School App</h1>
-  </Layout>;
+  return (
+    <Layout>
+      <Home />
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+      <h1>Sanskar School App</h1>
+    </Layout>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './index.scss';
 import App from './App';
